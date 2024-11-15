a68da4ae2b50be136bc325483fabb76bfeafde0b
ThanhNhunqq <httnhunq@gmail.com>
2024-11-15 22:55:02 +0700

    add Introduce

---
+++ KoiFarmShop.WebApplication/Pages/Introduce/gioi-thieu.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KoiFarmShop.WebApplication.Pages.Introduce
+{
+    public class gioi_thieu
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
