6f8487b3c13cc8048d9ac8ab53348219c458be48
ThanhNhunqq <httnhunq@gmail.com>
2024-11-18 10:51:25 +0700

    git by Thai , Last Git Update

--- KoiFarmShop.WebApplication/Pages/Payment/Cart.cshtml.cs
+++ KoiFarmShop.WebApplication/Pages/Payment/Cart.cshtml.cs
@@ -1,17 +1,17 @@
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.AspNetCore.Mvc.RazorPages;
-//using KoiFarmShop.Repositories.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using KoiFarmShop.Repositories.Entities;
 
-//namespace KoiFarmShop.WebApplication.Pages.Payment
-//{
-//    public class CartModel : PageModel
-//    {
-//        public List<CartItem> CartItems { get; set; }
-//        public decimal TotalPrice { get; set; }
+namespace KoiFarmShop.WebApplication.Pages.Payment
+{
+    public class CartModel : PageModel
+    {
+        public List<CartItem> CartItems { get; set; }
+        public decimal TotalPrice { get; set; }
 
-//        public void OnGet()
-//        {
-//            // Code to initialize CartItems and TotalPrice
-//        }
-//    }
-//}
+        public void OnGet()
+        {
+            // Code to initialize CartItems and TotalPrice
+        }
+    }
+}
