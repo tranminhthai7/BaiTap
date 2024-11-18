cf24cfc7184a50fdc0686eab4c2edb6e9a4a20ed
ThanhNhunqq <httnhunq@gmail.com>
2024-11-18 10:17:30 +0700

    update home, productcatrgory, promotion by thanh ngan

--- KoiFarmShop.WebApplication/Pages/Promotion/san-pham-khuyen-mai.cshtml.cs
+++ KoiFarmShop.WebApplication/Pages/Promotion/san-pham-khuyen-mai.cshtml.cs
@@ -1,27 +1,32 @@
+﻿using KoiFarmShop.Repositories.Entities;
+using KoiFarmShop.Repositories.Interfaces;
+using KoiFarmShop.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using KoiFarmShop.Services.Services;
-using KoiFarmShop.Repositories.Entities;
-using KoiFarmShop.Services.Interfaces;
 
-namespace KoiFarmShop.WebApplication.Pages.ProductCategory
+
+namespace KoiFarmShop.WebApplication.Pages.Promotion
 {
-	public class chi_tiet_san_phamModel : PageModel
+	public class san_pham_khuyen_maiModel : PageModel
 	{
-
 		private readonly IProductService _productService;
 		private readonly IPromotionService _promotionService;
+		private readonly IProductRepository _productRepository;
 
-		public chi_tiet_san_phamModel(IProductService productService, IPromotionService promotionService)
+		// Constructor duy nhất nhận DI
+		public san_pham_khuyen_maiModel(IProductService productService, IPromotionService promotionService, IProductRepository productRepository)
 		{
 			_productService = productService ?? throw new ArgumentNullException(nameof(productService));
 			_promotionService = promotionService ?? throw new ArgumentNullException(nameof(promotionService));
+			_productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
 		}
 
 		public IEnumerable<Product> Products { get; private set; }
 		public IEnumerable<KoiFarmShop.Repositories.Entities.Promotion> Promotions { get; private set; }
 
+		// Phương thức GET để lấy dữ liệu sản phẩm và khuyến mãi
 		public async Task OnGetAsync()
 		{
 			Products = await _productService.GetAllProductsAsync();
